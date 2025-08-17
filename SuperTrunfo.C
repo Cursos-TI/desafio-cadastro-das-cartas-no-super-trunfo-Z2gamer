b6e0eb4302f6a3178c3b0948ead20d5a383e410a
Enzzo Soares <enzzo.sda@gmail.com>
2025-08-17 14:25:57 +0000

    Projeto funal(desafio mestre)

--- SuperTrunfo.C
+++ SuperTrunfo.C
@@ -12,6 +12,7 @@ typedef struct {
     float area;
     float pib;
     int pontosTuristicos;
+
 } Carta;
 
 int main() {
@@ -72,6 +73,9 @@ int main() {
     float densidade2 = (float) carta2.populacao/carta2.area;
     float pibpercapita2 = (carta2.pib * 1000000000)/carta2.populacao;
 
+    float superPoder1 = (float) carta1.populacao + carta1.area + (carta1.pib*1000000000) + carta1.pontosTuristicos + pibpercapita1 - densidade1;
+    float superPoder2 = (float) carta2.populacao + carta2.area + (carta2.pib*1000000000) + carta2.pontosTuristicos + pibpercapita2 - densidade2;
+
     // Exibição das cartas
     printf("\n=== Cartas Cadastradas ===\n");
 
@@ -85,6 +89,7 @@ int main() {
     printf("Pontos Turísticos: %d\n", carta1.pontosTuristicos);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
     printf("PIB per capita: %.2f reais\n", pibpercapita1);
+    printf("Super Poder: %.2f \n", superPoder1);
 
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", carta2.estado);
@@ -96,6 +101,17 @@ int main() {
     printf("Pontos Turísticos: %d\n", carta2.pontosTuristicos);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
     printf("PIB per capita: %.2f reais\n", pibpercapita2);
+    printf("Super Poder: %.2f \n", superPoder2);
+
+    //Comparação das Cartas
+    printf("\n=== Compração das Cartas ===\n");
+    printf("%s\n", carta1.populacao > carta2.populacao ? "População: Carta 1 venceu(1)": "População;: Carta 2 venceu(0)" );
+    printf("%s\n", carta1.area > carta2.area ? "Área: Carta 1 venceu(1)": "Área: Carta 2 venceu(0)");
+    printf("%s\n", carta1.pib > carta2.pib ? "PIB: Carta 1 venceu(1)": "PIB: Carta 2 venceu(0)");
+    printf("%s\n", carta1.pontosTuristicos > carta2.pontosTuristicos ? "Pontos Turísticos: Carta 1 venceu(1)": "Pontos Turísticos: Carta 2 venceu(0)");
+    printf("%s\n", densidade1 < densidade2 ? "Densidade Populacional: Carta 1 venceu(1)": "Densidade Populacional: Carta 2 venceu(0)");
+    printf("%s\n", pibpercapita1 > pibpercapita2 ? "PIB per capita: Carta 1 venceu(1)": "PIB per capita: Carta 2 venceu(0)");
+    printf("%s\n", superPoder1 > superPoder2 ? "Super Poder: Carta 1 venceu(1)" : "Super Poder: Carta 2 venceu(0)");
 
     return 0;
 }
